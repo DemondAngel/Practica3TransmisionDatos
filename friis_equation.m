265a3135f02f9eca203a87e7b2a43de24827815c
DemondAngel <iarogo@hotmail.com>
2021-11-29 09:27:13 -0600

    feat: Shannon Capacity Matlab

--- friis_equation.m
+++ friis_equation.m
@@ -11,7 +11,7 @@ d = 0:0.01:20;
 r = [0.1 0.5 1 5 10 15 20];
 Prx = @(d) Ptx + Gtx + Grx + 20*log10(c/fc) - 20*log(4*pi*d);
 
-figure(1);
+friis=figure(1);
 plot(d, Prx(d));
 title('Friis Equation');
 xlabel('Distance (m)');
@@ -19,4 +19,6 @@ ylabel('Reception Power (W)');
 hold on
 plot(r,Prx(r),'r*');
 grid on;
+
+saveas(friis, 'images/friis.png')
 
--- shannon.m
+++ shannon.m
@@ -3,15 +3,15 @@ close all;
 clc;
 
 SNR= 0:0.01:20;
-
-snr=10.^(SNR/10);
 B=25e+03;
-
+snr=1*10.^(SNR/10);
 C = B*log2(1+snr);
 
-figure(1);
+shannonImage = figure(1);
 plot(SNR, C);
-title('Shannon');
+title('Shannon-Hartley Capacity');
 xlabel('SNR_{dB}');
 ylabel('Channel Capacity');
 grid on;
+
+saveas(shannonImage, 'images/shannon_capacity.png');
